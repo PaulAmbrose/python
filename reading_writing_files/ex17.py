c328e3b09eb9a3e6dc273664524f2506700025e4
PaulAmbrose <pablo_rose34@hotmail.com>
2022-02-22 19:12:04 +0000

    Add files via upload

---
+++ reading_writing_files/ex17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+infile = open(from_file)
+indata = infile.read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("All done")
+
+out_file.close()
+infile.close()
