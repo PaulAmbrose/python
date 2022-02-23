fa555ae93ab71e15f36cf638f12a796e1ff66420
PaulAmbrose <pablo_rose34@hotmail.com>
2022-02-23 19:27:42 +0000

    Add files via upload

---
+++ functions/ex18.py
@@ -0,0 +1,17 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I have no args")
+
+print_two("Paul", "Paula")
+print_two_again("Paul", "Paula")
+print_one("First!")
+print_none()
